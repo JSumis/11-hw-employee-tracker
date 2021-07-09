a6cf0458222fe3c42f1d484220bac58181d2a341
Joseph Sumis <Jsumis@comcast.net>
2021-07-08 22:08:16 -0500

    added else if to inquirer

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require ("mysql");
 const connection = mysql.createConnection ({
   host: "localhost", 
   user: "root",
-  password: "Password1"
+  password: "Password1",
   database: "employeeTrackerDB"
 })
 connection.connect()
--- server.js
+++ server.js
@@ -13,6 +13,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Force true to drop/recreate table(s) on every sync
-sequelize.sync({ force: true }).then(() => {
+// sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+// });
+
+
+inquirer.prompt([{
+  type: 'list',
+  message: 'What would you like to do?',
+  name: 'task',
+  choices: [
+    'View All Employees', 
+    'View All Employees by Department', 
+    'View All Employees by Manager', 
+    'Add Employee', 
+    'Remove Employee', 
+    'Update Employee Role', 
+    'Update Employee Manager', 
+    'View All Roles',
+    'Quit'
+  ]
+}]).then ((question) => {
+    console.log (question.task)
+
+    if (question.task === "View All Employees") {
+      console.log("Success!")
+      viewAllEmployees();
+    } else if (question.task === "View All Employees by Department") {
+      viewAllEmployeesByDepartment();
+    } else if (question.task === "View All Employees by Manager") {
+      viewAllEmployeesByManager();
+    } else if (question.task === "Add Employee") {
+      viewAddEmployee();
+    }
+
+    })
+
+    
+
